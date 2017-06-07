28ac3803367c7a6b27ae56f4413d47fb21e47aa4
Nathan Elg <elgn@students.wwwu.edu>
2017-06-07 15:33:56 -0600

    added register

--- www/src/router/index.js
+++ www/src/router/index.js
@@ -19,6 +19,11 @@ export default new Router({
 
     },
     {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
       path: '*',
       redirect: '/404'
     }
