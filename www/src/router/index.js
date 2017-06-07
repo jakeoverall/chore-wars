2f09d74065dea7b356a0426bb77c9b235fa70ad2
Nathan Elg <elgn@students.wwwu.edu>
2017-06-07 15:46:44 -0600

    fixed register

--- www/src/router/index.js
+++ www/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Login from '@/components/Login'
+import Register from '@/components/Register'
 
 Vue.use(Router)
 
