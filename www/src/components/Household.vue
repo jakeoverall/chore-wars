2ccd2c49e32aadf484b72badeb20cc0933b6e421
Nathan Elg <elgn@students.wwwu.edu>
2017-06-08 15:28:44 -0600

    added household component

---
+++ www/src/components/Household.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="household">
+
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'household',
+  data(){
+
+  },
+  computed:{},
+  methods:{},
+  components:{}
+}
+</script>
+
+
+<style scoped>
+
+</style>
